68e5377bbecba5017ff3c54a0f2f478130ce36ef
Matthew Koski <maccam912@gmail.com>
2023-05-19 16:56:55 -0500

    Update parse_xml_to_txt.py

--- parse_xml_to_txt.py
+++ parse_xml_to_txt.py
@@ -29,7 +29,6 @@ class WikiContentHandler(xml.sax.ContentHandler):
                 self.is_article = True
         elif self.current_tag == "text" and self.is_article and len(content) > 100:
             self.current_text += content.encode("ascii", errors="replace").decode("ascii", errors="replace").replace('[', '').replace(']', '')
-            self.current_text += ' '
 
 def parse_xml(xml_file_path, text_file_path):
     parser = xml.sax.make_parser()
