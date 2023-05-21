eb1f95bf31860fe9d6033c373e3db7788f585a7e
Matt Koski <maccam912@gmail.com>
2023-05-21 06:09:49 -0500

    xlnet style

--- main.py
+++ main.py
@@ -1,36 +1,78 @@
 import os
 import time
-from tokenizers import Tokenizer, models, pre_tokenizers, normalizers, trainers
+
 from datasets import load_dataset
+from tokenizers import (
+    Regex,
+    Tokenizer,
+    decoders,
+    models,
+    normalizers,
+    pre_tokenizers,
+    processors,
+    trainers,
+)
 
 DATASET = "lsb/simplewiki2023"
 TOKENIZER_OUTPUT_PATH = "tokenizer.json"
-VOCAB_SIZE = 4096
+VOCAB_SIZE = 512
 
-def log_step_start(step_name):
+
+def log_step_start(step_name: str) -> None:
     print(f"Starting {step_name}...")
 
-def log_step_end(step_name, start_time):
+
+def log_step_end(step_name: str, start_time: float) -> None:
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Finished {step_name} in {elapsed_time:.2f} seconds.")
 
-def main():
+
+def main() -> None:
     start_time = time.time()
 
     if not os.path.exists(TOKENIZER_OUTPUT_PATH):
         log_step_start("training tokenizer")
-        tokenizer = Tokenizer(models.Unigram())
-        tokenizer.pre_tokenizer = pre_tokenizers.BertPreTokenizer()
-        tokenizer.normalizer = normalizers.BertNormalizer()
 
-        trainer = trainers.UnigramTrainer(vocab_size=VOCAB_SIZE, special_tokens=["[UNK]"])
+        tokenizer = Tokenizer(models.Unigram())
+
+        tokenizer.normalizer = normalizers.Sequence(
+            [
+                normalizers.Replace(Regex(r"[^ -~]+"), "<unk>"),
+                normalizers.Replace("``", '"'),
+                normalizers.Replace("''", '"'),
+                normalizers.NFKD(),
+                normalizers.StripAccents(),
+                normalizers.Replace(Regex(" {2,}"), " "),
+            ]
+        )
+
+        tokenizer.pre_tokenizer = pre_tokenizers.Metaspace()
+
+        cls_token_id = tokenizer.token_to_id("<cls>")
+        sep_token_id = tokenizer.token_to_id("<sep>")
+
+        tokenizer.post_processor = processors.TemplateProcessing(
+            single="$A:0 <sep>:0 <cls>:2",
+            pair="$A:0 <sep>:0 $B:1 <sep>:1 <cls>:2",
+            special_tokens=[("<sep>", sep_token_id), ("<cls>", cls_token_id)],
+        )
+
+        tokenizer.decoder = decoders.Metaspace()
+
+        special_tokens = ["<cls>", "<sep>", "<unk>", "<pad>", "<mask>", "<s>", "</s>"]
+        trainer = trainers.UnigramTrainer(
+            vocab_size=4096, special_tokens=special_tokens, unk_token="<unk>"
+        )
 
         dataset = load_dataset(DATASET)
-        tokenizer.train_from_iterator(dataset["train"]["text"], trainer=trainer, length=len(dataset["train"]))
+        tokenizer.train_from_iterator(
+            dataset["train"]["text"], trainer=trainer, length=len(dataset["train"])
+        )
 
         tokenizer.save(TOKENIZER_OUTPUT_PATH)
         log_step_end("training tokenizer", start_time)
+
 
 if __name__ == "__main__":
     main()
